c58d99284d6ef1bfa0c24c5cdf3e48b8a8aa0235
ContronPAnda <rajtel99@gmail.com>
2018-10-09 19:43:21 -0400

    Finished Board and CheckerBoard classes

---
+++ src/checkers/CheckerBoard.java
@@ -0,0 +1,52 @@
+package checkers;
+
+import java.awt.Color;
+import java.util.List;
+
+import general.Board;
+import general.Piece;
+import general.Side;
+import general.Utils;
+
+public class CheckerBoard extends Board 
+{
+
+	public CheckerBoard(int size)
+	{
+		super(size);
+
+		for (int x = 0; x < size; x++)
+		{
+			for (int y = 0; y < size; y++)
+			{
+				if ((x+y) % 2 == 0)
+				{
+					if (x <= 2)
+						board[x][y] = new CheckerPiece(Side.TOP, Color.BLACK);
+					else if (x >= 5)
+						board[x][y] = new CheckerPiece(Side.BOTTOM, Color.RED);
+				}
+			}
+		}
+	}
+	
+	public void move(int[] from, int[] to)
+	{
+		Piece p = board[from[0]][from[1]];
+		List<int[]> valid = p.getMoves(board, from);
+		if(Utils.containsArray(valid, to))
+		{
+			List<int[]> captures = Utils.seqAverage(Utils.split(to));
+			for(int[] pos : captures)
+			{
+				if(!board[pos[0]][pos[1]].getColor().equals(p.getColor()))
+				{
+					board[pos[0]][pos[1]] = null;
+				}
+			}
+			
+			super.move(from, to);
+		}
+	}
+	
+}
--- src/general/Board.java
+++ src/general/Board.java
@@ -1,20 +1,20 @@
 package general;
 
 import java.awt.Color;
-import java.util.List;
 
 public class Board 
 {
-	private Piece[][] board;
+	protected Piece[][] board;
+	
+	public Board(int size)
+	{
+		board = new Piece[size][size];
+	}
 	
 	public void move(int[] from, int[] to)
 	{
-		Piece p = board[from[0]][from[1]];
-		List<int[]> valid = p.getMoves(board, from);
-		if(Utils.containsArray(valid, to))
-		{
-			
-		}
+		board[to[to.length-2]][to[to.length-1]] = board[from[0]][from[1]];
+		board[from[0]][from[1]] = null;
 	}
 	
 	public int countPieces(Color c)
@@ -25,5 +25,22 @@ public class Board
 				if(p.getColor().equals(c))
 					i++;
 		return i;
+	}
+	
+	public String toString()
+	{
+		String out = "";
+		for(Piece[] row : board)
+		{
+			for(Piece p : row)
+			{
+				if(p != null)
+					out += p;
+				else
+					out += "▢";
+			}
+			out += "\n";
+		}
+		return out;
 	}
 }
