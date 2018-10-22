30b445ed1d68606b26b4f4d7ae9abd5e6f8f565d
Logan Daniels <120LDaniels@frhsd.com>
2018-10-22 11:00:27 -0400

    Added Chess Methods

---
+++ src/chess/Bishop.java
@@ -0,0 +1,22 @@
+package chess;
+
+import java.awt.Color;
+import java.util.List;
+
+import general.Piece;
+import general.Side;
+
+public class Bishop extends Piece{
+
+	public Bishop(Side s, Color c) {
+		super(s, c);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<int[]> getMoves(Piece[][] board, int[] current) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/chess/King.java
@@ -0,0 +1,22 @@
+package chess;
+
+import java.awt.Color;
+import java.util.List;
+
+import general.Piece;
+import general.Side;
+
+public class King extends Piece {
+
+	public King(Side s, Color c) {
+		super(s, c);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<int[]> getMoves(Piece[][] board, int[] current) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
