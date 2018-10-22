9ab399f60c6a456dca129fe300e25d474bd41dde
ContronPAnda <rajtel99@gmail.com>
2018-10-22 11:01:13 -0400

    Added some GUI stuff

--- src/checkers/GraphicsInterface.java
+++ src/checkers/GraphicsInterface.java
@@ -1,5 +1,8 @@
 package checkers;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JFrame;
 
 import general.Board;
@@ -17,7 +20,13 @@ public class GraphicsInterface
 		
 		
 		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		game.getContentPane().add(board.getPanel());
+		game.getContentPane().setLayout(new GridBagLayout());
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		
+		game.getContentPane().add(board.getPanel(), c);
 		game.pack();
 		
 		game.setVisible(true);
--- src/general/BoardSquare.java
+++ src/general/BoardSquare.java
@@ -2,6 +2,8 @@ package general;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.JPanel;
 
@@ -12,9 +14,13 @@ public class BoardSquare extends JPanel
 {
 	/** This class is probably never going to be serialized, but Eclipse made me add this anyway. */
 	private static final long serialVersionUID = -1840139402010540234L;
+	/** The color used when this square is selected. */
+	private static final Color SELECTED_COLOR = Color.YELLOW;
 	
 	/** The piece currently sitting in this square. */
 	private Piece piece;
+	/** The color to be used for this piece when it is not selected. */
+	private Color backColor;
 
 	/**
 	 * Creates a new BoardSquare.
@@ -23,6 +29,7 @@ public class BoardSquare extends JPanel
 	 */
 	public BoardSquare(Color c)
 	{
+		backColor = c;
 		setBackground(c);
 	}
 	
@@ -37,14 +44,59 @@ public class BoardSquare extends JPanel
 	}
 	
 	/**
+	 * Changes the background color to indicate that this square is selected.
+	 */
+	public void select()
+	{
+		setBackground(SELECTED_COLOR);
+	}
+	
+	/**
+	 * Reverts the background color to the original to indicate that this square is no longer selected.
+	 */
+	public void deselect()
+	{
+		setBackground(backColor);
+	}
+	
+	/**
 	 * Draws this panel, then draws the {@link Piece} on top of it, scaled to fit exactly in this square.
 	 */
 	public void paintComponent(Graphics g)
 	{
+//		int s = Math.min(getWidth(), getHeight());
+//		setSize(new Dimension(s, s));
+		
 		super.paintComponent(g);
 		if(piece != null)
 		{
 			piece.draw(this, g, 0, 0, getWidth(), getHeight());
 		}
+	}
+	
+//	public Dimension getPreferredSize()
+//	{
+//		Dimension d = super.getPreferredSize();
+//        Container c = getParent();
+//        if (c != null)
+//        {
+//            d = c.getSize();
+//        }
+//        else
+//        {
+//            return new Dimension(10, 10);
+//        }
+//        int w = (int) d.getWidth();
+//        int h = (int) d.getHeight();
+//        int s = (w < h ? w : h);
+//        return new Dimension(s, s);
+//	}
+	
+	private class ResizeListener extends ComponentAdapter
+	{
+        public void componentResized(ComponentEvent e)
+        {
+            // Recalculate the variable you mentioned
+        }
 	}
 }
